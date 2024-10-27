8015d291bd2b6463626d7ab8a737f6d7e4b32d5c
Gustavo Araujo <121242417+gustavofda@users.noreply.github.com>
2024-10-27 15:49:40 -0300

    Irão receber atualizações

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function toggleMode() {
+/*function toggleMode() {
     document.body.classList.toggle("dark-mode");
-}
+}*/
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const body = document.body;
+
+toggleBtn.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
+
